3f184456c3ad24497edeae54478b0945bb7e2e44
Chitender Kumar <chitenderkumar.16@gmail.com>
2020-07-21 11:53:39 +0530

    Configuring build pipeline

---
+++ build.sh
@@ -0,0 +1,17 @@
+echo "printing environment variables..."
+printenv
+
+echo "Configuring aws default profile..."
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile awsdeploy
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile awsdeploy
+aws configure set region eu-west-1 --profile awsdeploy
+
+echo "printing aws profiles..."
+echo "$(<~/.aws/credentials )"
+
+echo "Maven Building"
+mvn clean -DskipTests package -Pdist
+
+echo "Sending build to s3"
+aws s3 cp distro/target/apache-atlas-3.0.0-SNAPSHOT-server.tar.gz s3://atlan-public/atlas/
