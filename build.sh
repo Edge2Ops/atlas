29a65270c6998cd15e4a46af971483f6cb084920
Nityananda Gohain <nityanandagohain@gmail.com>
2020-07-27 15:57:19 +0530

    first test

--- build.sh
+++ build.sh
@@ -26,6 +26,10 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile awsdeploy
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile awsdeploy
 aws configure set region eu-west-1 --profile awsdeploy
 
+# download local maven repository
+ls ~/.m2
+
+exit
 echo "printing aws profiles..."
 echo "$(<~/.aws/credentials )"
 
