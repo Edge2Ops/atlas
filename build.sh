6c7577469337cdbef45265c102dfd80277d067e6
Nishant Arora <8947356+nshntarora@users.noreply.github.com>
2020-09-28 18:24:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,4 +40,4 @@ echo "Maven Building"
 mvn -pl '!addons/sqoop-bridge,!addons/sqoop-bridge-shim' -DskipTests package -Pdist
 
 echo "Sending build to s3"
-aws s3 cp distro/target/apache-atlas-3.0.0-SNAPSHOT-server.tar.gz s3://atlan-build-artifacts/multitenant/
+aws s3 cp distro/target/apache-atlas-3.0.0-SNAPSHOT-server.tar.gz s3://atlan-build-artifacts/multitenant/ --acl public-read
