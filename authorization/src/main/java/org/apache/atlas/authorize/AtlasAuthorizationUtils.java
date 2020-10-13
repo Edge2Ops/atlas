de1d323524dfeead372d701dba9a3601adc486a4
Nishant Arora <na.nishantarora@gmail.com>
2020-10-13 13:13:29 +0530

    apikey service-account handling fix

--- authorization/src/main/java/org/apache/atlas/authorize/AtlasAuthorizationUtils.java
+++ authorization/src/main/java/org/apache/atlas/authorize/AtlasAuthorizationUtils.java
@@ -242,6 +242,11 @@ public class AtlasAuthorizationUtils {
             System.out.println(e);
         }
 
+        // If realm exists, the token is of an Apikey service account, prepend realm.
+        if (realm != "" && auth != null && !auth.getName().startsWith("service-account-apikey")) {
+            return auth != null ? realm + "_" + auth.getName() : "";
+        }
+
         // If realm exists, the token is not of a service account, prepend realm.
         if (realm != "" && auth != null && !auth.getName().startsWith("service-account-")) {
             return auth != null ? realm + "_" + auth.getName() : "";
